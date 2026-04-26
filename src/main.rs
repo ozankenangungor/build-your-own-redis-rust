f9e77bea2a9539012b6bf7dabf27d6c989c83bcd
ozankenangungor <s220204044@ankarabilim.edu.tr>
2026-04-26 17:34:52 +0300

    Bind TCP listener to port 6379

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,12 @@
-#![allow(unused_imports)]
-use std::net::TcpListener;
+use anyhow::Result;
+use tokio::net::TcpListener;
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
+#[tokio::main]
+async fn main() -> Result<()> {
+    let listener = TcpListener::bind("127.0.0.1:6379").await?;
 
-    // Uncomment the code below to pass the first stage
-    //
-    // let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
-    //
-    // for stream in listener.incoming() {
-    //     match stream {
-    //         Ok(_stream) => {
-    //             println!("accepted new connection");
-    //         }
-    //         Err(e) => {
-    //             println!("error: {}", e);
-    //         }
-    //     }
-    // }
+    loop {
+        let (_stream, addr) = listener.accept().await?;
+        println!("accepted new connection from {addr}");
+    }
 }
