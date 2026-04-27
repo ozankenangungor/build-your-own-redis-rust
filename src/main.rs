de19c611097c525b42e2e33592eb828f8d72db92
ozankenangungor <s220204044@ankarabilim.edu.tr>
2026-04-27 13:40:25 +0300

    Respond to PING with PONG

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,27 @@
 use anyhow::Result;
-use tokio::net::TcpListener;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use tokio::net::{TcpListener, TcpStream};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let listener = TcpListener::bind("127.0.0.1:6379").await?;
 
     loop {
-        let (_stream, addr) = listener.accept().await?;
+        let (stream, addr) = listener.accept().await?;
         println!("accepted new connection from {addr}");
+
+        if let Err(e) = handle_connection(stream).await {
+            eprintln!("connection error: {e}");
+        }
     }
+}
+
+async fn handle_connection(mut stream: TcpStream) -> Result<()> {
+    let mut buf = [0u8; 512];
+
+    // The command is ignored for now; every request gets a hardcoded PONG.
+    stream.read(&mut buf).await?;
+    stream.write_all(b"+PONG\r\n").await?;
+
+    Ok(())
 }
