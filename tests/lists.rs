31e594169eca502673953e41c5b447d40b2014bf
ozankenangungor <s220204044@ankarabilim.edu.tr>
2026-05-01 17:41:54 +0300

    Cover appending to an existing list

--- tests/lists.rs
+++ tests/lists.rs
@@ -12,6 +12,29 @@ fn creates_a_list_on_the_first_push() {
 }
 
 #[test]
+fn appends_to_an_existing_list() {
+    let server = Server::start();
+    let mut client = server.connect();
+
+    for length in 1..=3 {
+        client.send(&["RPUSH", "list_key", &format!("element{length}")]);
+        client.expect_reply(&format!(":{length}\r\n"));
+    }
+}
+
+#[test]
+fn keeps_a_list_per_key() {
+    let server = Server::start();
+    let mut client = server.connect();
+
+    client.send(&["RPUSH", "one", "element"]);
+    client.expect_reply(":1\r\n");
+
+    client.send(&["RPUSH", "other", "element"]);
+    client.expect_reply(":1\r\n");
+}
+
+#[test]
 fn rejects_a_push_onto_a_string() {
     let server = Server::start();
     let mut client = server.connect();
